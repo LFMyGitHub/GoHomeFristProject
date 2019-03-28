5bf7b719fc27d3b3904f946a2622f7fdf903a807
longfeng <545945521@qq.com>
2019-03-28 10:58:14 +0800

    模块集成

--- moduleMain/src/main/java/com/example/modulemain/MainActivity.java
+++ moduleMain/src/main/java/com/example/modulemain/MainActivity.java
@@ -201,6 +201,7 @@ public class MainActivity extends BaseActivity implements OnBannerListener {
     @Override
     public void OnBannerClick(int position) {
         Toast.makeText(getApplicationContext(), "你点击了：" + position, Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
